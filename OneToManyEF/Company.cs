20580ac788517d0e823f26eddbe526325a870f47
AyvazovaAyten <you@example.com>
2022-12-02 10:19:19 +0400

    Добавьте файлы проекта.

---
+++ OneToManyEF/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace OneToManyEF
+{
+    public class Company
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; } = string.Empty;
+        private List<Employee> Employees { get; set; }
+    }
+}
